d9a8d98a568bbc944142b9cf5d1a37c418e84425
tksrl0379 <tksrl0379@naver.com>
2020-04-19 10:49:34 +0900

    Initial Commit

---
+++ LocalCurrency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalCurrency
+//
+//  Created by a1111 on 2020/04/19.
+//  Copyright © 2020 SIMPARK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
